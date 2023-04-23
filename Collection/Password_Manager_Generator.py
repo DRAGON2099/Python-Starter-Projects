71f2689caa9e62735c7a83277dedb1da29522bcd
Swarnabh Paul <dragon2099xt@gmail.com>
2023-04-23 21:06:41 +0530

    Update Password_Manager_Generator.py

--- Collection/Password_Manager_Generator.py
+++ Collection/Password_Manager_Generator.py
@@ -1,16 +1,19 @@
-# Code for managing the Password
+import time
+import random
+import string
+
 
 master_pwd = input("Enter Master password : ")
 
 
-def view(x):
+def view():
 
     with open('passwords.txt', 'r') as f:
         for line in f.readlines():
             print(line)
 
 
-def enter(x):
+def enter():
     name = input("Enter Name of Account : ")
     pwd = input("Enter Password : ")
 
@@ -18,22 +21,47 @@ def enter(x):
         f.write("Name of Account : " + name + "\n" + f"Password of {name} Account : " + pwd + "\n")
 
 
-def generate(x):
-    pass
+def generate_password():
+    #Password will by default contain letters
+    number_choice = input("Do you want to include numbers in the password? (Y/N) : ")
+    special_choice = input("Do you want to include special characters in the password? (Y/N) : ")
 
+    letters = string.ascii_letters
+    numbers = string.digits
+    specials = string.punctuation
+
+    characters = letters
+    if number_choice == 'Y' or number_choice == 'Yes' or number_choice == 'YES' or number_choice == 'yes':
+        characters += numbers
+    if special_choice == 'Y' or special_choice == 'Yes' or special_choice == 'YES' or special_choice == 'yes':
+        characters += specials    
+
+    gen_pwd = []
+    min_length = int(input("Enter Minimum length of Password needed : "))
+    max_length = int(input("Enter Maximum length of Password needed : "))
+    length = random.randint(min_length,max_length)
+    for idx in range (length):
+        element = random.choice(characters)
+        gen_pwd.append(element)
+
+    final_pwd = ''
+    for i in range (len(gen_pwd)):
+        final_pwd = final_pwd + gen_pwd[i]
+
+    return final_pwd    
 
 while True:
-    mode = input("Would you like to view existing passwords or enter a new one or have a new one generated or quit"
-                 '? (view/enter/generate/quit) : ').lower()
-    if mode == 'view':
-        view(1)
-    elif mode == 'enter':
-        enter(1)
-    elif mode == 'generate':
-        generate(1)
-    elif mode == 'quit':
+    mode = input("Would you like to view existing passwords or enter a new one or have a new one generated or quit" '? (view/enter/generate/quit) : ').lower()
+    if mode == 'view' or mode == 'View' or mode == 'VIEW':
+        view()
+    elif mode == 'enter' or mode == 'Enter' or mode == 'ENTER':
+        enter()
+    elif mode == 'generate' or mode == 'Generate' or mode == 'GENERATE':
+        gener_pwd = generate_password()
+        print(f"Generated Password is : {gener_pwd}")
+    elif mode == 'quit' or mode == 'Quit' or mode == 'QUIT':
         print("Have a nice day!")
         break
     else:
-        print("Invalid input")
+        print("ERROR! Invalid input!")
         continue
